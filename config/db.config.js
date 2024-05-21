546ee872ef81e073a0e5f7b0e69f285c67e15eba
WealthAbraham <wealthabram@gmail.com>
2024-05-21 12:09:16 +0100

    db.config.js

--- config/db.config.js
+++ config/db.config.js
@@ -3,4 +3,3 @@ const mongoos = require("mongoose");
 var uri = "mongodb+srv://WEALTHABRAHAM:Mololuwa2.@primary.tbvdsze.mongodb.net/?retryWrites=true&w=majority&appName=Primary";
 
 exports.dbconnect = async () => await mongoos.connect(uri);
-
